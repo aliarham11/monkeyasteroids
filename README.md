361a6cd4e15ba452593d0323fc86d94f75e5af99
Yoga Pratama Aliarham <yoga.aliarham@tiket.com>
2019-11-13 18:17:01 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MonkeyAsteroids
+
+## We're here to catch the firefly
+This is repository for Datarangers github hands on
---
+++ catch.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+files = []
+print('fireflies cathed :')
+for r, d, f in os.walk('fireflies'):
+    for file in f:
+        f_path = os.path.join(r, file)
+        with open(f_path) as f_file:
+            content = f_file.read()
+            print(content)
+        
+        time.sleep(1)
